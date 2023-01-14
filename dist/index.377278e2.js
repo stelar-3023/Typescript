104fd7d5c6c2c292236298c44d78ae4eb5406319
Steve Larsen <slarsen-3@att.net>
2023-01-14 13:48:37 -0600

    added parcel and start scripts

---
+++ dist/index.377278e2.js
@@ -0,0 +1,9 @@
+// const sum = (a: number, b: number): number => {
+//     return a + b;
+// }
+//
+// console.log(sum(20, 30));
+//
+// console.log("Hello World from TypeScript");
+
+//# sourceMappingURL=index.377278e2.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// const sum = (a: number, b: number): number => {
+//     return a + b;
+// }
+//
+// console.log(sum(20, 30));
+//
+// console.log("Hello World from TypeScript");
